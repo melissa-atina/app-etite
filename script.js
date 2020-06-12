724b4a6c76ef3bbcf367c0fa765cbf39e6034cef
atinachang <atinachang@gmail.com>
2020-06-12 16:56:59 -0400

    added variables to get results_found, randomize noOfPages found and then set to start page; called 2nd API

--- script.js
+++ script.js
@@ -2,15 +2,6 @@ const foodApp = {};
 foodApp.apiKey = "ef7bb4cac402a95c8319040f2339e230";
 foodApp.apiUrl = "https://developers.zomato.com/api/v2.1/search";
 
-//shuffel array function
-function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        const temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-}
 // make empty array for our API response
 const response = [];
 
@@ -26,15 +17,58 @@ foodApp.foodEntity = (cuisine) => {
         data: {
             city_id: 89,
             cuisines: `${cuisine}`,
-            // data: 'json',
-            // count: 2
-            // q: "toronto"
+            count: 1,
         },
     }).then((result) => {
-        // console.log(result.restaurants)
-        foodApp.displayFood(result)
+        // console.log(result)
+        
+        const resultpp = result.results_found;
+        console.log(resultpp)
+        //here we want to look at result.results_found and store in a const
+
+        //divide const by 2 and should return # of various pages in the cuisine array
+        const noOfpages = (Math.ceil(resultpp / 2));
+        console.log(noOfpages)
+        //we want a fully randomized number of restaurants
+
+        //1- # of pages <50 or >50
+        //we want to assign the random page based on if it provides less than or  more than 50
+        let randomPage = 1;
+        console.log(noOfpages)
+        if (noOfpages >= 50) {
+            randomPage = (Math.ceil(Math.random()* 50));
+            //this one would apply to all restaurants that have greater than 50 pages
+        } else {
+            randomPage = (Math.ceil(Math.random()* noOfpages));
+            console.log('else')
+        }
+        console.log(randomPage)
+        //since we can start on any page we want, we want to generate a random page number
+        //create a 2nd API call and pass the randomizeStart as start param
+        $.ajax({
+            url: foodApp.apiUrl,
+            method: 'GET',
+            dataType: 'json',
+            headers: {
+                "user-key": foodApp.apiKey,
+            },
+            data: {
+                city_id: 89,
+                cuisines: `${cuisine}`,
+                start: randomPage,
+                count: 2,
+            },
+        }).then((result) => {
+            console.log(result)
+            foodApp.displayFood(result)
+
         })
-    }
+
+        //we will get 20 random restaurants from our API
+        //then randomly select 2
+    })
+}
+        
 
 
     foodApp.displayFood = (result) => {
@@ -50,7 +84,6 @@ foodApp.foodEntity = (cuisine) => {
             // push title to response array we created earlier
             response.push(title, rating, address)
         })
-        shuffleArray(response)
     }
 
     foodApp.updateTitle = (subject) => {
